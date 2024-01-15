d3604a71d0ca500ec52e97761bf05000732c0eff
anshu-rai018 <anshu.rai018@gmail.com>
2024-01-15 21:25:03 +0530

    first commit

---
+++ day1/calculateSumProductGivenTwoNumber.java
@@ -0,0 +1,19 @@
+package day1;
+
+public class calculateSumProductGivenTwoNumber {
+
+    public static void main(String[] args) {
+        
+        int a,b,sum = 0,product = 0;
+
+        a = 10;
+        b = 20;
+
+        sum = a + b;
+        product = a * b;
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Product = " + product);
+    }
+    
+}
---
+++ day1/java_first_program.java
@@ -0,0 +1,13 @@
+package day1;
+
+/**
+ * java_first_program
+ * welcome to java coding
+ */
+public class java_first_program {
+
+    public static void main(String[] args) {
+        
+        System.out.print("wellcome to java Coding");
+    }
+}
